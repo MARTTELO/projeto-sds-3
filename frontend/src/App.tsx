ba575d8287576e664ef1ae1d70dbbda0ab44e3c5
Marcelo Ludwig Junior <marttelo@outlook.com>
2021-05-03 22:35:11 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-primary">Olá Mundo</h1>
-    </div>);
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable />
+      </div>
+
+      <Footer />
+    </>
+  );
+
 }
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/MARTTELO/projeto-sds-3" target="_blank" rel="noreferrer">Marcelo Ludwig Junior</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/marttelo/" target="_blank" rel="noreferrer">@marttelo</a></small></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
