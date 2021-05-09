41d81150282fd57904d876441d72e0121ff06bb8
Marcelo Ludwig Junior <marttelo@outlook.com>
2021-05-09 01:08:33 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,34 +3,12 @@ import Footer from "components/Footer";
 import DataTable from "components/DataTable";
 import Barchart from "components/BarChart";
 import DonutChart from "components/DonutChart.tsx";
+import Routes from "Routes";
 
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-            <Barchart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Participação da Vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas Vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-
-      <Footer />
-    </>
+    <Routes />
   );
 
 }
