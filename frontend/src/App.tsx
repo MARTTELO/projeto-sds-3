8c4629a8deeb13f636c07b19da703f736fae5422
Marcelo Ludwig Junior <marttelo@outlook.com>
2021-05-09 03:36:26 -0300

    removendo imports sem uso

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import NavBar from "components/NavBar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import Barchart from "components/BarChart";
-import DonutChart from "components/DonutChart.tsx";
 import Routes from "Routes";
 
 
